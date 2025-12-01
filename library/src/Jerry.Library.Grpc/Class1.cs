f9fb2e059e47a1d862eb0ce75639d2c1a6c85f25
Mark Jungeblut <mark.jungeblut.development@gmail.com>
2025-12-01 14:50:46 +0100

    Add Nerdbank.GitVersioning package references

--- library/src/Jerry.Library.Grpc/Class1.cs
+++ library/src/Jerry.Library.Grpc/Class1.cs
@@ -1,6 +1,12 @@
-﻿namespace Jerry.Library.Grpc;
+// <copyright file="Class1.cs" company="Your Company">
+// Copyright (c) Your Company. All rights reserved.
+// </copyright>
 
+namespace Jerry.Library.Grpc;
+
+/// <summary>
+/// A placeholder class for the Jerry.Library.Grpc library.
+/// </summary>
 public class Class1
 {
-
 }
